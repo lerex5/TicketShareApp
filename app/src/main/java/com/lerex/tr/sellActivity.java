07d76c2b0a245c00b4a292f3e657e500c6fee005
lerex5 <aswin3519@gmail.com>
2019-10-08 17:01:34 +0530

    Event Class Designed

--- app/src/main/java/com/lerex/tr/sellActivity.java
+++ app/src/main/java/com/lerex/tr/sellActivity.java
@@ -6,14 +6,33 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.Date;
+
+class event {
+    public String eventName,cost;
+    public Date eventDate;
+    public int noOfTickets;
+
+    public  event(String eventName,String cost,Date eventDate,int noOfTickets){
+        this.eventDate=eventDate;
+        this.eventName=eventName;
+        this.cost=cost;
+        this.noOfTickets=noOfTickets;
+    }
+}
+
 public class sellActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_sell);
+
+
     }
 }
