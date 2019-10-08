32fe11f1e7fdba0e5b8ae34c6a7787cf97f618d9
Neha1521 <curdle39@gmail.com>
2019-10-08 15:06:12 +0530

    Fullscreen Mode

--- app/src/main/java/com/lerex/tr/MainActivity.java
+++ app/src/main/java/com/lerex/tr/MainActivity.java
@@ -1,8 +1,12 @@
 package com.lerex.tr;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         final Button signup=(Button)findViewById(R.id.sign_up);
         final EditText eU,ePh,eEa;
@@ -49,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
                 email=eEa.getText().toString();
                 u1 = new user1(u_name,ph_no,email);
                 mydb.child(ph_no).setValue(u1);
+                Intent intent=new Intent(MainActivity.this,sellActivity.class);
+                startActivity(intent);
             }
         });
 
