75271bc6f5b75e16616eac57e483300011ffbe99
lerex5 <aswin3519@gmail.com>
2019-10-09 20:24:19 +0530

    Added Fragment View

---
+++ app/src/main/java/com/lerex/tr/SignUp.java
@@ -0,0 +1,23 @@
+package com.lerex.tr;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class SignUp extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.signuplayout, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
