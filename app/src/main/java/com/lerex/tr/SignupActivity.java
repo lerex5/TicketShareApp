c898aa539878db68a5faa5020cbb9f2715e0bfc7
lerex5 <aswin3519@gmail.com>
2019-10-11 18:16:50 +0530

    Minor Changes#2

--- app/src/main/java/com/lerex/tr/SignupActivity.java
+++ app/src/main/java/com/lerex/tr/SignupActivity.java
@@ -12,16 +12,22 @@ import android.widget.EditText;
 public class SignupActivity extends AppCompatActivity {
 
 
-
+    private EditText email,password,retypepassword;
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);//FullScreening The Application
 
         setContentView(R.layout.activity_signup);
 
+        email = findViewById(R.id.etEmail);
+        password=findViewById(R.id.etPass);
+        retypepassword=findViewById(R.id.etRepass);
+        signup=findViewById(R.id.btnSignup);
 
     }
 }
