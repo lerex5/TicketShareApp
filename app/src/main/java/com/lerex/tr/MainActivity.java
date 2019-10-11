4f4d0d34d34deedef48890369e467eb3f937e618
Neha1521 <curdle39@gmail.com>
2019-10-11 15:53:40 +0530

    SIGN UP

--- app/src/main/java/com/lerex/tr/MainActivity.java
+++ app/src/main/java/com/lerex/tr/MainActivity.java
@@ -1,35 +1,23 @@
 package com.lerex.tr;
 
-import androidx.appcompat.app.AppCompatActivity;
-//import androidx.fragment.app.Fragment;
-//import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-//import android.widget.EditText;
 import android.widget.TextView;
-
-/*
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.widget.EditText;
 import com.google.firebase.database.IgnoreExtraProperties;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.lerex.tr.R;
-*/
+
 
 public class MainActivity extends AppCompatActivity {
-
-//    String e_id,pas;
-
-    TextView sign=findViewById(R.id.tvSignup);
- //   EditText email_id=findViewById(R.id.email);
-   // EditText pass=findViewById(R.id.password);
-    Button login=findViewById(R.id.login);
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +30,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        /*login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-
-                //pass.setError("Password doesn't match!");
-            }
-        });
-        sign.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,SignupActivity.class);
-                startActivity(intent);
-            }
-        });*/
     }
 }
--- app/src/main/java/com/lerex/tr/SignupActivity.java
+++ app/src/main/java/com/lerex/tr/SignupActivity.java
@@ -9,12 +9,7 @@ import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
 
-    String pas,repas;
 
-    //EditText email=findViewById(R.id.etEmail);
-    EditText pass=findViewById(R.id.etPass);
-    EditText repass=findViewById(R.id.etRepass);
-    Button sign=findViewById(R.id.btnSignup);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +17,5 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
 
-
-        sign.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pas=pass.getText().toString();
-                repas=repass.getText().toString();
-                if(!pas.equals(repas))
-                {
-                    repass.setError("Password doesn't match");
-                }
-            }
-        });
     }
 }
