2b174d6a23e9a480923b0bd2b8e5600466f9fdc9
Neha1521 <curdle39@gmail.com>
2019-10-12 22:48:48 +0530

    listview

--- app/src/main/java/com/lerex/tr/sellActivity.java
+++ app/src/main/java/com/lerex/tr/sellActivity.java
@@ -8,8 +8,10 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,6 +19,7 @@ import android.view.WindowManager;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 class event {
@@ -42,9 +45,12 @@ public class sellActivity extends AppCompatActivity {
         EditText eDate = findViewById(R.id.etDate);
         EditText eCost = findViewById(R.id.etCost);
         EditText eNo = findViewById(R.id.etNumber);
-        event newEvent = new event(eName.getText().toString(),eCost.getText().toString(),(Date) eDate.getText(),Integer.valueOf(eNo.getText().toString()));
+       // event newEvent = new event(eName.getText().toString(),eCost.getText().toString(),(Date) eDate.getText(),Integer.valueOf(eNo.getText().toString()));
+
+
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,14 +62,19 @@ public class sellActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sell);
 
         sellerLayout=findViewById(R.id.sellerLa);
+
         Button Add=findViewById(R.id.btnAdd);
+        final ListView lv=findViewById(R.id.lvTickets);
+        final ArrayList<String> Tickets=new ArrayList<>();
+        final ArrayAdapter<String> TickAdapter=new ArrayAdapter<>(this,R.layout.activity_listview,Tickets);
+
 
         Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //instantiate the popup.xml layout file
                 LayoutInflater layoutInflater = (LayoutInflater) sellActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View customView = layoutInflater.inflate(R.layout.addticket,null);
+                final View customView = layoutInflater.inflate(R.layout.addticket,null);
 
                 //instantiate popup window
                 aPop = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
@@ -72,16 +83,27 @@ public class sellActivity extends AppCompatActivity {
                 //display the popup window
                 aPop.showAtLocation(sellerLayout, Gravity.CENTER, 0, 0);
 
+                final EditText eName = customView.findViewById(R.id.etName);
                 Button Add1=customView.findViewById(R.id.btnAdd1);
                 Add1.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        addTickets();
+                        //addTickets();
+
+                        String eN=eName.getText().toString();
                         aPop.dismiss();
+                        Tickets.add(eN);
+                        TickAdapter.notifyDataSetChanged();
+                        lv.setAdapter(TickAdapter);
+
                     }
                 });
+
+
             }
         });
+
+
 
     }
 }
