6fbb8def990c816267cd9c4ac7365da1d3bc0367
lerex5 <aswin3519@gmail.com>
2019-10-13 06:50:20 +0530

    Minor Changes #3

--- app/src/main/java/com/lerex/tr/MainActivity.java
+++ app/src/main/java/com/lerex/tr/MainActivity.java
@@ -7,19 +7,16 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 import com.google.firebase.database.IgnoreExtraProperties;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
