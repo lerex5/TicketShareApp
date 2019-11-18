eab14537f34c9810068eec997c9446034c419f7f
Neha1521 <curdle39@gmail.com>
2019-11-18 18:43:26 +0530

    Ticket details in a list

--- app/src/main/java/com/lerex/tr/ViewManager.java
+++ app/src/main/java/com/lerex/tr/ViewManager.java
@@ -7,6 +7,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -25,7 +27,13 @@ public class ViewManager extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//FullScreening The Application
+
         setContentView(R.layout.activity_view_manager);
+
+
 
         tinydb = new TinyDB(this);
         new GetmovieResults().execute();
