46b99359c0914e530a0babcd6485b238694cd7b5
lerex5 <aswin3519@gmail.com>
2019-12-03 14:56:20 +0530

    Manifest Correction

--- app/src/main/java/com/lerex/tr/FragHome.java
+++ app/src/main/java/com/lerex/tr/FragHome.java
@@ -31,9 +31,6 @@ public class FragHome extends AppCompatActivity {
 
     private ViewPager viewPager;
     private String TAG = FragHome.class.getSimpleName();
-    private ArrayList<String> movies=new ArrayList<>();
-    private ArrayList<String> cities=new ArrayList<>();
-    private TinyDB tinydb;
     private TextView bg;
     private BottomNavigationView bottomNavigationView;
 
@@ -44,28 +41,6 @@ public class FragHome extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);//FullScreening The Application
-
-        tinydb = new TinyDB(this);
-        tinydb.deleteImage("Movies");
-        //new GetmovieResults().execute();
-
-
-        if(tinydb.getListString("Cities").isEmpty()) {
-            CitiesJSON CityList = new CitiesJSON(getResources(), R.raw.cities);
-            String JsonCityList = CityList.getJsonString();
-            try {
-                //JSONObject jsonObj = new JSONObject(JsonCityList);
-                JSONArray citiesJSON = new JSONArray(JsonCityList);
-                for (int i = 0; i < citiesJSON.length(); i++) {
-                    JSONObject c = citiesJSON.getJSONObject(i);
-                    String title = c.getString("City");
-                    cities.add(title);
-                }
-                tinydb.putListString("Cities", cities);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
 
         setContentView(R.layout.activity_frag_home);
 
@@ -106,82 +81,6 @@ public class FragHome extends AppCompatActivity {
         }
     };
 
-    private class GetmovieResults extends AsyncTask<Void, Void, Void> {
-        int pages = 1;
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-           // Toast.makeText(FragHome.this, "Json Data is downloading", Toast.LENGTH_LONG).show();
-        }
 
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
-            // Making a request to url and getting response
-
-            for(int j=1;j<=pages;j++) {
-                String url = "https://api.themoviedb.org/3/movie/now_playing?region=IN&page=" + j + "&language=en-US&api_key=cdb6543f56d4ae849f71ed220c46a080";
-                String jsonStr = sh.makeServiceCall(url);
-
-                Log.e(TAG, "Response from url: " + j);
-
-                if (jsonStr != null) {
-                    try {
-                        JSONObject jsonObj = new JSONObject(jsonStr);
-
-                        // Getting JSON Array node
-                        JSONArray movieResults = jsonObj.getJSONArray("results");
-                        pages = jsonObj.getInt("total_pages");
-                        // looping through All movieResults
-                        for (int i = 0; i < movieResults.length(); i++) {
-                            JSONObject c = movieResults.getJSONObject(i);
-                            String title = c.getString("title");
-
-                            // tmp hash map for single contact
-                            HashMap<String, String> moviesReleased = new HashMap<>();
-
-                            // adding each child node to HashMap key => value
-                            moviesReleased.put("title", title);
-                            movies.add(moviesReleased.get("title"));
-
-                        }
-                        tinydb.putListString("Movies", movies);
-                        System.out.println(tinydb.getListString("Movies"));
-                    } catch (final JSONException e) {
-                        Log.e(TAG, "Json parsing error: " + e.getMessage());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), "Json parsing error: " + e.getMessage(),
-                                        Toast.LENGTH_LONG).show();
-                            }
-                        });
-
-                    }
-
-                } else {
-                    Log.e(TAG, "Couldn't get json from server.");
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Couldn't get json from server. Check LogCat for possible errors!",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
-                }
-            }
-                return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-
-            //To Add Modifications For Drop Down List Box
-           // Toast.makeText(FragHome.this, "Json Data downloaded", Toast.LENGTH_LONG).show();
-        }
-
-    }
 }
 
