625d4dcdf4e4a8836780d525491e570c2ebcfb80
lerex5 <aswin3519@gmail.com>
2019-12-05 15:07:25 +0530

    Location Displays As Toast And Location Is To Be Enabled Manually

--- app/src/main/java/com/lerex/tr/FragHome.java
+++ app/src/main/java/com/lerex/tr/FragHome.java
@@ -2,6 +2,7 @@ package com.lerex.tr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -81,6 +82,13 @@ public class FragHome extends AppCompatActivity {
         }
     };
 
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
 
 }
 
