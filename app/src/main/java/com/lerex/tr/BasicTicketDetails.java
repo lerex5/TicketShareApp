bab532018049ff322a124a9fd904c7927c683c06
Neha1521 <curdle39@gmail.com>
2019-12-14 22:22:17 +0530

    Add tickets windows

---
+++ app/src/main/java/com/lerex/tr/BasicTicketDetails.java
@@ -0,0 +1,14 @@
+package com.lerex.tr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicTicketDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_ticket_details);
+    }
+}
---
+++ app/src/main/java/com/lerex/tr/CostDetails.java
@@ -0,0 +1,14 @@
+package com.lerex.tr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CostDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cost_details);
+    }
+}
